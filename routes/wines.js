66b232bbfe23453e7572b0782d66da306e7e802c
martinsjr1976 <martinsjr1976@gmail.com>
2021-04-15 21:21:01 +0000

    routes

---
+++ routes/wines.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+
+})
+
+//Getting One
+router.get('/:id', (req, res) => {
+
+    
+})
+
+//Creating One
+router.post('/', (req, res) => {
+
+    
+})
+
+//Updating One
+router.patch('/', (req, res) => {
+
+    
+})
+
+//Delete One
+router.delete('/:id', (req, res) => {
+
+    
+})
+
+module.exports = router
